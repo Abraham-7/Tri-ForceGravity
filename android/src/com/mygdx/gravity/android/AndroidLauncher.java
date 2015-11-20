8abd5649ec7216c7565ed5bcaf4476b378da59c5
Abraham-7 <noria0540@hotmail.com>
2015-11-20 10:30:50 -0500

    Initial commit

---
+++ android/src/com/mygdx/gravity/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.gravity.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.gravity.triforce_gravity;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new triforce_gravity(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/gravity/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.gravity.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.gravity.triforce_gravity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new triforce_gravity(), config);
+	}
+}
